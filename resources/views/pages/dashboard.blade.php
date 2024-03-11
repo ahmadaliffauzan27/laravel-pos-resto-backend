476f23827b5a413c50351d7e9debb85db08c522b
ahmadaliffauzan27 <ahmadaliffauzan@gmail.com>
2024-03-12 02:57:57 +0700

    refactor: update count and color

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -92,57 +92,16 @@ $data = $orders->pluck('total_amount')->toArray();
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Total Discount</h4>
+                                <h4>Total Order</h4>
                             </div>
                             <div class="card-body">
-                                <?php echo $total_discount; ?>
+                                <?php echo $total_order; ?>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-md-12 col-12 col-sm-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Statistics</h4>
-                            <div class="card-header-action">
-                                <div class="btn-group">
-                                    <a href="#" class="btn btn-primary">Week</a>
-                                    <a href="#" class="btn">Month</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="statistic-details mt-sm-4">
-                                <!-- Tampilkan total order di sini -->
-                                <div class="statistic-details-item">
-                                    <div class="detail-value">Total Order:</div>
-                                    <div class="detail-name"><?php echo $total_order; ?></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12 col-12 col-sm-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Recent Activities</h4>
-                        </div>
-                        <div class="card-body">
-                            <ul class="list-unstyled list-unstyled-border">
-                                <li class="media">
-                                    <!-- Daftar aktivitas -->
-                                </li>
-                            </ul>
-                            <div class="pt-1 pb-1 text-center">
-                                <a href="#" class="btn btn-primary btn-lg btn-round">View All</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
+            </div>
         </section>
     </div>
 @endsection
