d563487e576fc9f0333ac962da2db6ff99d89d14
ahmadaliffauzan27 <ahmadaliffauzan@gmail.com>
2024-07-10 14:00:33 +0700

    refactor: delete service charge

--- resources/views/pages/report/pdf.blade.php
+++ resources/views/pages/report/pdf.blade.php
@@ -25,12 +25,12 @@
     <table>
         <thead>
             <tr>
-                <th>ID</th>
+                <th>No</th>
                 <th>Pembayaran Pembeli</th>
                 <th>Subtotal</th>
                 <th>Pajak</th>
                 <th>Diskon</th>
-                <th>Biaya Layanan</th>
+                {{-- <th>Biaya Layanan</th> --}}
                 <th>Total Harga</th>
                 <th>Metode Pembayaran</th>
                 <th>Total Item</th>
@@ -46,7 +46,7 @@
                     <td>{{ 'Rp ' . number_format($order->sub_total, 0, ',', '.') }}</td>
                     <td>{{ 'Rp ' . number_format($order->tax, 0, ',', '.') }}</td>
                     <td>{{ 'Rp ' . number_format($order->discount, 0, ',', '.') }}</td>
-                    <td>{{ 'Rp ' . number_format($order->service_charge, 0, ',', '.') }}</td>
+                    {{-- <td>{{ 'Rp ' . number_format($order->service_charge, 0, ',', '.') }}</td> --}}
                     <td>{{ 'Rp ' . number_format($order->total, 0, ',', '.') }}</td>
                     <td>{{ $order->payment_method }}</td>
                     <td>{{ $order->total_item }}</td>
